17fa968fc357796fdc51424df6dd6ae984270dfa
Aroa Granja Iglesias <aroagranjaiglesias@gmail.com>
2025-07-24 19:34:00 +0200

    Change left icons + added font

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -19,34 +19,50 @@ function Grid() {
   };
 
   const guardarColores = () => {
-        const coloresGuardados = cuadrado.map((isActive, index) => ({
-            index,
-            color: isActive ? "bg-pink-400" : "bg-gray-300"
-        }));
-        localStorage.setItem("coloresGrid", JSON.stringify(coloresGuardados));
-    };
-    const cargarColores = () => {
-        const coloresGuardados = localStorage.getItem("coloresGrid");
-        if (coloresGuardados) {
-            const coloresArray = JSON.parse(coloresGuardados);
-            const nuevoCuadrado = initialState.map((_, index) => {
-                const colorObj = coloresArray.find(c => c.index === index);
-                return colorObj ? colorObj.color === "bg-pink-400" : false;
-            });
-            setCuadrado(nuevoCuadrado);
-        }
+    const coloresGuardados = cuadrado.map((isActive, index) => ({
+      index,
+      color: isActive ? "bg-pink-400" : "bg-gray-300",
+    }));
+    localStorage.setItem("coloresGrid", JSON.stringify(coloresGuardados));
+  };
+  const cargarColores = () => {
+    const coloresGuardados = localStorage.getItem("coloresGrid");
+    if (coloresGuardados) {
+      const coloresArray = JSON.parse(coloresGuardados);
+      const nuevoCuadrado = initialState.map((_, index) => {
+        const colorObj = coloresArray.find((c) => c.index === index);
+        return colorObj ? colorObj.color === "bg-pink-400" : false;
+      });
+      setCuadrado(nuevoCuadrado);
     }
+  };
 
   return (
     <>
-    <h1 className="text-center">Create your polaroid pixel art</h1>
+      <h1 className="text-center font-montserrat p-10">
+        Create your polaroid pixel art
+      </h1>
       <div className="flex flex-row items-center justify-center gap-12 pt-8">
-         <div className="flex flex-col items-center justify-center space-x-4 mt-4 p-2 gap-12">
-          <button className="bg-gray-300 p-3 rounded" onClick={guardarColores}>Guardar</button>
-          <button className="bg-gray-300 p-3 rounded" onClick={borrarColores}>
-            Limpiar
-          </button>
-          <button className="bg-gray-300 p-3 rounded" onClick={cargarColores}>Dibujo anterior</button>
+        <div className="flex flex-col items-center justify-center mt-4 p-2 gap-12">
+          <img
+            src="/document-download.png"
+            className="w-10 h-10"
+            alt="image"
+            onClick={guardarColores}
+          />
+          <img
+            src="/eraser.png"
+            className="w-10 h-10"
+            alt="image"
+            onClick={borrarColores}
+          />
+
+          <img
+            src="/back-square.png"
+            className="w-10 h-10"
+            alt="image"
+            onClick={cargarColores}
+          />
         </div>
         <div className="flex flex-col items-center justify-center p-4 ">
           <div className="grid grid-cols-12 gap-1 p-4 bg-gray-200 rounded-lg shadow-lg">
@@ -62,15 +78,13 @@ function Grid() {
           </div>
         </div>
 
-       
         <div className="flex flex-col items-center justify-center space-x-4">
-          
           <div className="flex flex-col gap-12">
-          <button className="bg-blue-500 p-3 rounded w-10 h-10" ></button>
-          <button className="bg-gray-500 p-3 rounded w-10 h-10" ></button>
-          <button className="bg-green-500 p-3 rounded w-10 h-10" ></button>
-          <button className="bg-yellow-500 p-3 rounded w-10 h-10" ></button>
-        </div>
+            <button className="bg-blue-500 p-3 rounded w-10 h-10"></button>
+            <button className="bg-gray-500 p-3 rounded w-10 h-10"></button>
+            <button className="bg-green-500 p-3 rounded w-10 h-10"></button>
+            <button className="bg-yellow-500 p-3 rounded w-10 h-10"></button>
+          </div>
         </div>
       </div>
     </>

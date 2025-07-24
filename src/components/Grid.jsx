683bc2e9d6508b4892bc4eb1397b575f2570a25d
Aroa Granja Iglesias <aroagranjaiglesias@gmail.com>
2025-07-24 19:23:10 +0200

    Update Grid.jsx: added save drawing + load last drawing with localStorage

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -18,16 +18,35 @@ function Grid() {
     setCuadrado(initialState);
   };
 
+  const guardarColores = () => {
+        const coloresGuardados = cuadrado.map((isActive, index) => ({
+            index,
+            color: isActive ? "bg-pink-400" : "bg-gray-300"
+        }));
+        localStorage.setItem("coloresGrid", JSON.stringify(coloresGuardados));
+    };
+    const cargarColores = () => {
+        const coloresGuardados = localStorage.getItem("coloresGrid");
+        if (coloresGuardados) {
+            const coloresArray = JSON.parse(coloresGuardados);
+            const nuevoCuadrado = initialState.map((_, index) => {
+                const colorObj = coloresArray.find(c => c.index === index);
+                return colorObj ? colorObj.color === "bg-pink-400" : false;
+            });
+            setCuadrado(nuevoCuadrado);
+        }
+    }
+
   return (
     <>
     <h1 className="text-center">Create your polaroid pixel art</h1>
       <div className="flex flex-row items-center justify-center gap-12 pt-8">
          <div className="flex flex-col items-center justify-center space-x-4 mt-4 p-2 gap-12">
-          <button className="bg-gray-300 p-3 rounded">Guardar</button>
+          <button className="bg-gray-300 p-3 rounded" onClick={guardarColores}>Guardar</button>
           <button className="bg-gray-300 p-3 rounded" onClick={borrarColores}>
             Limpiar
           </button>
-          <button className="bg-gray-300 p-3 rounded">Dibujo anterior</button>
+          <button className="bg-gray-300 p-3 rounded" onClick={cargarColores}>Dibujo anterior</button>
         </div>
         <div className="flex flex-col items-center justify-center p-4 ">
           <div className="grid grid-cols-12 gap-1 p-4 bg-gray-200 rounded-lg shadow-lg">

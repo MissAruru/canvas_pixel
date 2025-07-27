c6d7f2d0aeabcc3abc4d234cb442aec048823d5c
Aroa Granja Iglesias <aroagranjaiglesias@gmail.com>
2025-07-27 11:58:51 +0200

    added react-colorful + fix css

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import { HexColorPicker } from "react-colorful";
+
 
 function Grid() {
   // Primero creo los cuadrados que serán el canvas del proyecto
 
-  const initialState = new Array(156).fill(false);
+  const initialState = new Array(224).fill(false);
   const [cuadrado, setCuadrado] = useState(initialState);
+  const [color, setColor] = useState("#aabbcc")
+  const [mostrarSeleccionado, setmostrarSeleccionado] = useState(false);
+  const toggleSeleccion = () => setmostrarSeleccionado((prev) => !prev);
 
   // Luego hacemos la fórmula para que al hacer clic en un cuadrado este se pinte de un color (por ahora, rosa)
   const pintarCuadrado = (index) => {
@@ -39,11 +44,11 @@ function Grid() {
 
   return (
     <>
-      <h1 className="text-center font-montserrat p-10">
+      <h1 className="text-center text-lg font-montserrat p-10 text-shadow-lg" >
         Create your polaroid pixel art
       </h1>
       <div className="flex flex-row items-center justify-center gap-12 pt-8">
-        <div className="flex flex-col items-center justify-center mt-4 p-2 gap-12">
+        <div className="flex flex-col items-center justify-center p-2 gap-12">
           <img
             src="/document-download.png"
             className="w-10 h-10"
@@ -65,12 +70,12 @@ function Grid() {
           />
         </div>
         <div className="flex flex-col items-center justify-center p-4 ">
-          <div className="grid grid-cols-12 gap-1 p-4 bg-gray-200 rounded-lg shadow-lg">
+          <div className="grid grid-cols-16 gap-1 p-4 bg-white-200 rounded-sm shadow-lg pb-20 border-black border-2 ">
             {cuadrado.map((isActive, index) => (
               <button
                 key={index}
                 onClick={() => pintarCuadrado(index)}
-                className={`w-5 h-5 ${
+                className={`w-4 h-4 ${
                   isActive ? "bg-pink-400" : "bg-gray-300"
                 } transition-colors duration-300`}
               ></button>
@@ -80,12 +85,17 @@ function Grid() {
 
         <div className="flex flex-col items-center justify-center space-x-4">
           <div className="flex flex-col gap-12">
-            <button className="bg-blue-500 p-3 rounded w-10 h-10"></button>
+            <button className="p-3 rounded w-10 h-10" onClick={() => setmostrarSeleccionado(!mostrarSeleccionado)} style={{ backgroundColor: color }} ></button>
             <button className="bg-gray-500 p-3 rounded w-10 h-10"></button>
-            <button className="bg-green-500 p-3 rounded w-10 h-10"></button>
-            <button className="bg-yellow-500 p-3 rounded w-10 h-10"></button>
+            <button className="bg-gray-500 p-3 rounded w-10 h-10"></button>
+            <button className="bg-gray-500 p-3 rounded w-10 h-10"></button>
           </div>
         </div>
+         {mostrarSeleccionado && (
+        <div style={{ marginTop: "10px" }}>
+          <HexColorPicker color={color} onChange={setColor} />
+        </div>
+      )}
       </div>
     </>
   );
